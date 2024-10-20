7512a69afb9544ae5dba387b42dca73cdc7d4007
saiiraw <ivanstalmahov@gmail.com>
2024-10-20 21:55:27 +0300

    solve first task

--- tasks/seconds_to_str.py
+++ tasks/seconds_to_str.py
@@ -16,4 +16,17 @@ def seconds_to_str(seconds: int) -> str:
         >> seconds_to_str(93600)
         01d02h00m00s
     """
-    raise NotImplementedError
+    day = seconds // (24 * 60 * 60)
+    seconds -= (day * 24 * 60 * 60)
+    hours = seconds // (60 * 60)
+    seconds -= (hours * 60 * 60)
+    minu = seconds // 60
+    seconds -= (minu * 60)
+    sec = seconds
+    if day == 0:
+        if hours == 0:
+            if minu == 0:
+                return f'{sec}s'
+            return f'{minu}m{sec}s'
+        return f'{hours}h{minu}m{sec}s'
+    return f'{day}d{minu}m{sec}s'
